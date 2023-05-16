23c3360eebb32fe30403d318dbb14ff3294ec1ca
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 23:44:23 +0200

    fix .html

--- server.js
+++ server.js
@@ -11,25 +11,7 @@ const http = require('http')
 
 const server = http.createServer(app)
 
-
-var publicdir = __dirname + '/public';
-
-app.use(function(req, res, next) {
-  if (req.path.indexOf('.') === -1) {
-    var file = publicdir + req.path + '.html';
-    fs.exists(file, function(exists) {
-      if (exists)
-        req.url += '.html';
-      next();
-    });
-  }
-  else
-    next();
-});
-app.use(express.static(publicdir));
-
-//app.use(express.static(__dirname + "/public"));
-//app.use(express.static(__dirname + "/public"), {extensions:['html']})
+app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + '/static', { dotfiles: 'allow' }))
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
